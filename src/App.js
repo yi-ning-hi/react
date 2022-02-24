9d6f281a5928d61686298e57ca7881d660b2b957
Yi-Ning <ning19950212@gmail.com>
2022-02-24 11:09:42 +0800

    表單元素

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ function App() {
   const [inputText, setInputText] = useState('');
   const [textareaText, setTextareaText] = useState('');
 
+  //radio group
+  const [radioValue, setRadioValue] = useState('');
+  const foodOptions = ['排骨飯', '雞腿飯', '牛肉麵'];
+  // select
+  const [selectedValue, setSelectedValue] = useState('');
+
   return (
     <>
       <h1>可控表單元素</h1>
@@ -21,6 +27,39 @@ function App() {
           setTextareaText(e.target.value);
         }}
       />
+      <h2>選項按鈕</h2>
+      {foodOptions.map((v, i) => {
+        return (
+          <div key={i}>
+            <input
+              type="radio"
+              value={v}
+              id={'foodOption' + i}
+              checked={v === radioValue}
+              onChange={(e) => {
+                setRadioValue(e.target.value);
+              }}
+            />
+            <label htmlFor={'foodOption' + i}>{v}</label>
+          </div>
+        );
+      })}
+      <h2>下拉選單</h2>
+      <label for="car">選擇車子：</label>
+      <select
+        name="cars"
+        id="cars"
+        value={selectedValue}
+        onChange={(e) => {
+          setSelectedValue(e.target.value);
+        }}
+      >
+        <option value="">請選擇</option>
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="opel">Opel</option>
+        <option value="audi">Audi</option>
+      </select>
     </>
   );
 }
