fb0190d75a6b111ccb51326bf8ab2c4d0d4e7394
Yi-Ning <ning19950212@gmail.com>
2022-03-02 11:03:56 +0800

    3.JQuery整合-讀react狀態

--- src/components/JqueryTestRefs.js
+++ src/components/JqueryTestRefs.js
@@ -1,21 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import $ from 'jquery';
 
 function JqueryTestRefs(props) {
+  const [total, setTotal] = useState(0);
   const titleRef = useRef(null);
   const buttonRef = useRef(null);
-  //didMount
+  //didMount+didUpdate
   useEffect(() => {
     //jquery code
-    console.log($(titleRef.current).text());
+    // console.log($(titleRef.current).text());
+
+    //移除原事件
+
+    //加入新事件
     $(buttonRef.current).on('click', () =>
-      alert('Hello' + $(titleRef.current).text())
+      alert('Hello' + $(titleRef.current).text() + '目前total狀態=' + total)
     );
-  }, []);
+  }, [total]);
   return (
     <>
       <h1 ref={titleRef}>React</h1>
       <button ref={buttonRef}>Click me</button>
+      <h1 onClick={() => setTotal(total + 1)}>{total}</h1>
     </>
   );
 }
